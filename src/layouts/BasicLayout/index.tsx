ba33100c826d0331a6c33de7a15ca1e4ba78a1a0
zhihuang <2584939303@qq.com>
2025-02-11 14:47:46 +0800

    题目详情 + 题库题目详情

---
+++ src/layouts/BasicLayout/index.tsx
@@ -0,0 +1,172 @@
+"use client";
+import {
+  GithubFilled,
+  LogoutOutlined,
+  SearchOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { ProLayout } from "@ant-design/pro-components";
+import { Divider, Dropdown, Input, message, theme } from "antd";
+import React, { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+
+import GlobalFooter from "@/components/GlobalFooter";
+import Image from "next/image";
+import { AppDispatch, RootState } from "@/stores";
+import { useDispatch, useSelector } from "react-redux";
+import menuAccess from "@/access/menuAccess";
+import { userLogoutUsingPost } from "@/api/userController";
+import { setLoginUser } from "@/stores/loginUser";
+import { DEFAULT_USER } from "@/constants/user";
+import {menus} from "../../../config/menu";
+import SearchInput from "@/layouts/BasicLayout/components/SearchInput";
+
+
+
+
+interface Props {
+  children: React.ReactNode;
+}
+
+/**
+ * 通用布局
+ * @param children
+ * @constructor
+ */
+export default function BasicLayout({ children }: Props) {
+  // listQuestionBankVoByPageUsingPost({}).then((res) => {
+  //   console.log(res);
+  // });
+  const pathname = usePathname();
+  const loginUser = useSelector((state: RootState) => state.loginUser);
+  const dispatch = useDispatch<AppDispatch>();
+  const router = useRouter();
+  /**
+   * 用户注销
+   */
+  const userLogout = async () => {
+    try {
+      await userLogoutUsingPost();
+      message.success("已退出登录");
+      dispatch(setLoginUser(DEFAULT_USER));
+      router.push("/user/login");
+    } catch (e) {
+      message.error("操作失败，" + e.message);
+    }
+    return;
+  };
+
+  return (
+    <div
+      id="basicLayout"
+      style={{
+        height: "100vh",
+        overflow: "auto",
+      }}
+    >
+      <ProLayout
+        title="面试宝刷题平台"
+        layout="top"
+        logo={
+          <Image
+            src="/assets/logo.png"
+            height={32}
+            width={32}
+            alt="面试宝刷题网站"
+          />
+        }
+        location={{
+          pathname,
+        }}
+        avatarProps={{
+          src: loginUser.userAvatar || "/assets/logo.png",
+          size: "small",
+          title: loginUser.userName || "李明",
+          render: (props, dom) => {
+            if (!loginUser.id) {
+              return (
+                <div
+                  onClick={() => {
+                    router.push("/user/login");
+                  }}
+                >
+                  {dom}
+                </div>
+              );
+            }
+            return (
+              <Dropdown
+                menu={{
+                  items: [
+                    {
+                      key: "userCenter",
+                      icon: <UserOutlined />,
+                      label: "个人中心",
+                    },
+                    {
+                      key: "logout",
+                      icon: <LogoutOutlined />,
+                      label: "退出登录",
+                    },
+                  ],
+                  onClick: async (event: { key: React.Key }) => {
+                    const { key } = event;
+                    if (key === "logout") {
+                      userLogout();
+                    } else if (key === "userCenter") {
+                      router.push("/user/center");
+                    }
+                  },
+                }}
+              >
+                {dom}
+              </Dropdown>
+            );
+          },
+        }}
+        actionsRender={(props) => {
+          if (props.isMobile) return [];
+          return [
+            <SearchInput key="search" />,
+            <a
+              key="github"
+              // href="https://github.com/liyupi/mianshiya-next"
+              // target="_blank"
+            >
+              <GithubFilled key="GithubFilled" />
+            </a>,
+          ];
+        }}
+        headerTitleRender={(logo, title) => {
+          return (
+            <a
+              href="/" // 点击跳转到首页
+              // style={{ display: 'flex', alignItems: 'center', color: 'inherit' }}
+            >
+              {logo}
+              {title}
+            </a>
+          );
+        }}
+        // 渲染底部栏
+        footerRender={() => {
+          return <GlobalFooter />;
+        }}
+        onMenuHeaderClick={(e) => console.log(e)}
+        // 定义有哪些菜单
+        menuDataRender={() => {
+          return menuAccess(loginUser, menus);
+        }}
+        // 定义了菜单项如何渲染
+        menuItemRender={(item, dom) => (
+          <Link href={item.path || "/"} target={item.target}>
+            {dom}
+          </Link>
+        )}
+      >
+        {children}
+      </ProLayout>
+    </div>
+  );
+}
